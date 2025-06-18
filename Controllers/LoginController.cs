e5562e732c7ce6c34a5e4a48f0f1cfef8e1bfde2
lowtab777 <chikiker07@gmail.com>
2025-06-19 01:58:14 +0300

    setting up

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShortLinkBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("Returned value");
+        } 
+    }
+}
