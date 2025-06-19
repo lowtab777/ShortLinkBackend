8ff70f080421d9e7aeceec5e8f5da87dda6823f8
lowtab777 <chikiker07@gmail.com>
2025-06-19 16:06:35 +0300

    added DI

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 
 using Microsoft.EntityFrameworkCore;
+using ShortLinkBackend.Interfaces;
+using ShortLinkBackend.Repositories;
+using ShortLinkBackend.Services;
 using ShortLinkBackend.ShortLinkDbContext;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +17,12 @@ builder.Services.AddDbContext<ShortUrlDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConntection"));
 });
+
+builder.Services.AddScoped<IUserRespository, UserRepository>();
+builder.Services.AddScoped<IShortLinkRepositroy, ShortLinkRepository>();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IShortLinkRepositroy, ShortLinkRepository>();
 
 var app = builder.Build();
 
